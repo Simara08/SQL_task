79a1b2d5472636929bf9205579a7acc9f581d005
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-06-01 04:13:08 +0400

    Add files via upload

---
+++ APS.net/WebApplication1/WebApplication1/Controllers/StudentController1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    public class studentController1 : Controller
+    {
+        List<Student> stulist = new List<Student>()
+        {
+            new Student(){Stuid=1,Stuname="simara",Stuage=20},
+            new Student(){Stuid=2,Stuname="dilara",Stuage=22},
+            new Student(){Stuid=3,Stuname="albina",Stuage=21},
+            new Student(){Stuid=4,Stuname="garanfil",Stuage=18}
+        };
+        public IActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                ViewBag.model = stulist;
+                return View();
+            }
+            else
+            {
+                foreach(var stu in stulist)
+                {
+                    if (id == stu.Stuid)
+                    {
+                        TempData["id"] = stu.Stuid;
+                        TempData["name"] = stu.Stuname;
+                        TempData["age"] = stu.Stuage;   
+                    }
+                }
+                ViewResult view = new ViewResult();
+                view.ViewName = "Index";
+                return view;
+            }
+        }
+    }
+}
---
+++ APS.net/WebApplication1/WebApplication1/Model/Students.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Student
+    {
+        public int Stuid { get; set; }
+        public string Stuname { get; set; }
+        public int Stuage { get; set; }
+    }
+}
